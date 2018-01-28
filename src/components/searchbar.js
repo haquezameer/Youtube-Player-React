210e628c0d2141e9e1dad0d7e3814929fc07ca40
Zameer Haque <haque864@gmail.com>
2018-01-29 00:33:09 +0530

    Initial tooling setup(add eslint, prettier)

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 
+const arr = [1, 2, 4, 5, 6];
+const anotherarr = [1, 3, 4, 5, 6, 7, 8];
 class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = { term: '' };
   }
-
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onVideoSearch(term);
+  }
   render() {
     return (
-      <div className="search-bar" >
+      <div className="search-bar">
         <input
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)}
         />
       </div>
     );
-  }
-
-  onInputChange(term) {
-    this.setState({ term });
-    this.props.osnVideoSearch(term);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,46 +1,49 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
+import YTsearch from 'youtube-api-search';
 import SearchBar from './components/searchbar';
 import VideoList from './components/video_list';
-import YTsearch from 'youtube-api-search';
 import VideoDetail from './components/video_detail';
-import _ from 'lodash';
 
 const API_KEY = 'AIzaSyA27wkBL00MkQdFEBw308d7I7C0SnA54I0';
 
 class App extends Component {
-	
-	constructor(props){
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = { 
-			videos : [],
-			selectedVideo : null
-		};
-	
-	this.videoSearch('surfboards');
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
 
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTsearch({ key: API_KEY, term }, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term); }, 300);
+    return (
+      <div>
+        <SearchBar onVideoSearch={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
 }
 
-	videoSearch(term){
-		YTsearch({key : API_KEY, term : term},(videos) => {
-			this.setState({
-				videos : videos,
-				selectedVideo : videos[0]
-				});
-		});
-	}
-
-	render() {
-		const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
-	return (<div>
-		<SearchBar onVideoSearch = {videoSearch} />
-		<VideoDetail video={this.state.selectedVideo} />
-		<VideoList onVideoSelect = {selectedVideo => this.setState({selectedVideo})} 
-		videos={this.state.videos} />
-	</div>
-	); 
-	}
-}
-
+/* eslint-disable */
 ReactDOM.render(<App />, document.querySelector('.container'));
+/* eslint-enable */
